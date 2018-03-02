e4aebd10aa027885e4773164a385095c118b33df
mholt2587 <mkholt36@gmail.com>
2018-03-02 13:24:00 -0800

    location history

--- app/src/main/java/com/example/mholt2587/yelptest/MainActivity.java
+++ app/src/main/java/com/example/mholt2587/yelptest/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mholt2587.yelptest;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.squareup.okhttp.Call;
@@ -23,20 +25,84 @@ public class MainActivity extends AppCompatActivity {
 
    private ListView mListView;
 
+    Button getLL;
+
+    // GPSTracker class
+    GPSTracker gps;
+
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mContext = this;
 
+//        getLL = (Button)findViewById(R.id.getLL);
+//        getLL.setOnClickListener(new View.OnClickListener() {
+//            @Override            public void onClick(View view) {
+//
+//
+//                if (ContextCompat.checkSelfPermission(mContext,
+//                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//
+//                        && ActivityCompat.checkSelfPermission(mContext,
+//                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                    ActivityCompat.requestPermissions(MainActivity.this,
+//                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//
+//                } else {
+//                    Toast.makeText(mContext, "You need to have granted permission", Toast.LENGTH_SHORT).show();
+//                    gps = new GPSTracker(mContext, MainActivity.this);
+//
+//                    // Check if GPS enabled
+//
+//                    if (gps.canGetLocation()) {
+//
+//                        double latitude = gps.getLatitude();
+//                        double longitude = gps.getLongitude();
+//
+//                        // \n is for new line
+//
+//                        Toast.makeText(getApplicationContext(),
+//                                "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+//                    } else {
+//
+//                        // Can't get location.
+//
+//                        // GPS or network is not enabled.
+//
+//                        // Ask user to enable GPS/network in settings.
+//
+//                        gps.showSettingsAlert();
+//                    }
+//                }
+//
+//            }
+//        });
 
         mListView = (ListView) findViewById(R.id.list_view);
 
         String term = "restuarant";
         double latitude = 40.015;
         double longitude = -105.271;
-        final String yelpUrl = "https://api.yelp.com/v3/businesses/search?term=" + term + "&latitude=" + latitude +  "&longitude=" + longitude;
 
+
+
+        gps = new GPSTracker(mContext, MainActivity.this);
+
+        if (gps.canGetLocation()) {
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+        }
+        else {
+            gps.showSettingsAlert();
+        }
+
+        final String yelpUrl = "https://api.yelp.com/v3/businesses/search?term=" + term + "&latitude=" + latitude +  "&longitude=" + longitude;
+        Log.d(TAG, String.valueOf(latitude));
+        Log.d(TAG, String.valueOf(longitude));
 
 
         OkHttpClient client = new OkHttpClient();
@@ -94,6 +160,57 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Main UI code is running!");
 
     }
+
+//    @Override    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//
+//        switch (requestCode) {
+//            case 1: {
+//                // If request is cancelled, the result arrays are empty.
+//
+//                if (grantResults.length > 0
+//
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+//                    // permission was granted, yay! Do the
+//                    // contacts-related task you need to do.
+//                    gps = new GPSTracker(mContext, MainActivity.this);
+//
+//
+//                    // Check if GPS enabled
+//
+//                    if (gps.canGetLocation()) {
+//
+//                        double latitude = gps.getLatitude();
+//                        double longitude = gps.getLongitude();
+//
+//                        // \n is for new line
+//
+//                        Toast.makeText(getApplicationContext(),
+//                                "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+//                    } else {
+//                        // Can't get location.
+//
+//                        // GPS or network is not enabled.
+//
+//                        // Ask user to enable GPS/network in settings.
+//
+//                        gps.showSettingsAlert();
+//                    }
+//
+//                } else {
+//
+//                    // permission denied, boo! Disable the
+//
+//                    // functionality that depends on this permission.
+//                    Toast.makeText(mContext, "You need to grant permission", Toast.LENGTH_SHORT).show();
+//                }
+//                return;
+//            }
+//        }
+//    }
+
+
 
     private void getCurrentDetails(String jsonData) throws JSONException {
 
